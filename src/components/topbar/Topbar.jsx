d519d8f1b62ecfcdc080f9f87a2b42755c84e328
Sharon Wasker <sharonwasker@gmail.com>
2021-05-26 19:49:38 +0300

    26.5.21 sections, topBar, menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,30 @@
-import './topbar.scss'
+import './topbar.scss';
+import { Face, MailOutline } from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="topbar">
-            
+        <div className={ "topbar " + (menuOpen && "active") }>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">SW</a>
+                    <div className="itemContainer">
+                        <Face className="icon"/>
+                        <span>0546914164</span>
+                    </div>
+                    <div className="itemContainer"> 
+                        <MailOutline className="icon"/>
+                        <span>sharonwasker@gmail.com</span>
+                    </div>
+                </div>
+                <h1>sharon wasker</h1>
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen) }>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
