2eb24e64d62a6968edbe9e5505db139e00b363be
Sharon Wasker <sharonwasker@gmail.com>
2021-06-06 16:03:43 +0300

    6.6.21 works section

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,24 @@ import './works.scss'
 export default function Works() {
     return (
         <div className="works" id="works">
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src={process.env.PUBLIC_URL + "/assets/mobile.png"} alt="" />
+                                </div>
+                                <h2>title</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                                   illo corporis voluptas sed fuga, ea, laboriosam adipisci iure!
+                                   </p>
+                            </div>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
