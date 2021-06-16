55aed6cb39a30de47114aa9d192d68aa6461d98a
Sharon Wasker <sharonwasker@gmail.com>
2021-06-16 23:32:00 +0300

    16.6.21 contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,30 @@
+
+import { useState, setState } from "react" ;
 import "./contact.scss"
 
 export default function Contact() {
+
+    const [ message, setMessage ] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    };
+
     return (
         <div className="contact" id="contact">
+            <div className="left">
+                <img src="/sharon-wasker-fed-developer/assets/shake.svg" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">SEND</button>
+                    {message && <span>Thank's Bro/Sista, I'll Reply ASAP ;) </span>}
+                </form>
+            </div>
             
         </div>
     )
